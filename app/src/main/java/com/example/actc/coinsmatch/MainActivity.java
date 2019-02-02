4c2b910ff78fcbecf7c8346ce6a53716b99bb757
RandSaleh <rand_nn@hotmail.com>
2019-02-02 16:28:27 +0200

    add tags to keep track

--- app/src/main/java/com/example/actc/coinsmatch/MainActivity.java
+++ app/src/main/java/com/example/actc/coinsmatch/MainActivity.java
@@ -6,16 +6,22 @@ import android.view.View;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+    boolean flag=false; // 0 if its yello tern
     /// The process for this method is to drop the image view off the screaan
     // then set the recourcse image for the image view
     //  drop it back again !
     public void dropIn(View view) {
         ImageView counter = (ImageView) view;
         counter.setTranslationY(-1500);
-        counter.setImageResource(R.drawable.goldcoin);
+        if (flag==false) {
+            counter.setImageResource(R.drawable.goldcoin);
+            flag = true;
+        }
+        else {
+            counter.setImageResource(R.drawable.coinred);
+            flag=false;
+        }
         counter.animate().translationY(30).setDuration(500);
-      //  counter.setTranslationY(1500);
-
     }
 
     @Override
